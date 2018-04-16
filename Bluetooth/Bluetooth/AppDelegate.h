a9bec1ff5c3d1c4e9a9308a67139a8b9fe8c62f0
Gabriellamm <379713491@qq.com>
2018-04-16 15:51:05 +0800

    init

---
+++ Bluetooth/Bluetooth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bluetooth
+//
+//  Created by Gabriella on 2018/4/16.
+//  Copyright © 2018年 Gabriella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bluetooth/Bluetooth/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bluetooth
+//
+//  Created by Gabriella on 2018/4/16.
+//  Copyright © 2018年 Gabriella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
